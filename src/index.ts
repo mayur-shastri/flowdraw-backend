a086ac71d37e66430941f2103918159c55af416f
Mau_2993 <shastrimayur2993@gmail.com>
2025-08-01 19:17:22 +0530

    Fix: dotenv fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
+import express from 'express';
 import userRouter from './routes/user.route';
 import diagramRouter from './routes/diagram.route';
 import invitationRouter from './routes/collabInvite.route';
